2e8d2d7ff81f7753ec2a6ddb1e1ac25e9b7cadad
ClintAfton <112364123+ClintAfton2003@users.noreply.github.com>
2023-03-09 20:18:03 +0700

    Update about_controller.js

--- controller/about_controller.js
+++ controller/about_controller.js
@@ -2,6 +2,6 @@ const Profile = require('../models/about_model');
 
 async function index(req, res, next) {
   const profiles = await Profile.find({});
-  res.render('components/about', { title: 'Our CV2', page: req.url, profile: profiles[0] });
+  res.render('components/about', { title: 'Our CV2 - About', page: req.url, profile: profiles[0] });
 }
 module.exports = { index };
