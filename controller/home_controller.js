4bb872546b8dee4fd18178d33a0f68384337b52c
ClintAfton2003 <clintafton@gmail.com>
2023-03-10 08:23:06 +0700

    Update contact

--- controller/home_controller.js
+++ controller/home_controller.js
@@ -1,8 +1,15 @@
 const Profile = require('../models/about_model');
+const Contact = require('../models/contact_model');
 
 async function index(req, res, next) {
   const profiles = await Profile.find({});
+  const contacts = await Contact.find({});
 
-  res.render('index', { title: 'Our CV2', page: req.url, profile: profiles[0] });
+  res.render('index', {
+    title: 'Our CV2',
+    page: req.url,
+    profile: profiles[0],
+    contact: contacts[0],
+  });
 }
 module.exports = { index };
